def2b42a853d99bbc3e6a7331cfb18795163fd8a
AbdullahShaaban <abdalahshaban@yahoo.com>
2022-02-26 22:44:27 +0200

    deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/"; //local
-// const url = "https://memories-mern-project-app1.herokuapp.com/"; //heroku
+// const url = "http://localhost:5000/"; //local
+const url = "https://memories-mern-stack-1.herokuapp.com/"; //heroku
 
 const API = axios.create({ baseURL: url });
 
